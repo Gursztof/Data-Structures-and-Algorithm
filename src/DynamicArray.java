7adaf4365a0b7c12a1f287d5aee2ca893451ddd2
Gursztof <krzyskurek08@gmail.com>
2025-12-16 19:08:40 +0100

    Implement linear search

--- src/DynamicArray.java
+++ src/DynamicArray.java
@@ -1,2 +1,75 @@
 public class DynamicArray {
+    private int size;
+    private int capacity;
+    private Object[] data;
+
+    public DynamicArray() {
+        this.size = 0;
+        this.capacity = 5;
+        data = new Object[capacity];
+    }
+
+    public DynamicArray(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        data = new Object[capacity];
+    }
+
+    public void add(Object element) {
+        if (size == capacity) {
+            grow();
+        }
+        data[size] = element;
+        size++;
+    }
+
+    public void remove(int index) {
+        if (isEmpty() || index < 0 || index >= size) { return; }
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        size--;
+    }
+
+    public void removeLast() {
+        data[size - 1] = null;
+        size--;
+    }
+
+    public Object get(int index) {
+        return data[index];
+    }
+
+    private void grow() {
+        int newCapacity = capacity * 2;
+        Object[] newArray = new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = data[i];
+        }
+        capacity = newCapacity;
+        data = newArray;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty()) {
+            return "[]";
+        }
+        StringBuilder stringToReturn = new StringBuilder("[");
+        for (int i = 0; i < size; i++) {
+            stringToReturn.append(data[i] + ", ");
+        }
+        stringToReturn.delete(stringToReturn.length() - 2, stringToReturn.length());
+        stringToReturn.append("]");
+
+        return stringToReturn.toString();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,29 @@
 void main() {
-  LinkedList ll = new LinkedList();
+  DynamicArray dyn = new DynamicArray();
 
-  ll.isEmpty();
-  ll.addFirst("Mag");
-  System.out.println(ll);
-  ll.addLast("Alan");
-  System.out.println(ll);
-  ll.addLast("George");
-  System.out.println(ll);
-  ll.addFirst("Joe");
-  System.out.println(ll);
-  System.out.println(ll.get(1));
-  System.out.println(ll.contains("George"));
-  System.out.println(ll.indexOf("Joe"));
+  dyn.add("Joe");
+  System.out.println(dyn);
+  dyn.add("Mag");
+  dyn.add("Alan");
+  System.out.println(dyn);
+  dyn.remove(2);
+  System.out.println(dyn);
+  dyn.removeLast();
+  System.out.println(dyn);
+  System.out.println(dyn.get(0));
+
+//  ll.isEmpty();
+//  ll.addFirst("Mag");
+//  System.out.println(ll);
+//  ll.addLast("Alan");
+//  System.out.println(ll);
+//  ll.addLast("George");
+//  System.out.println(ll);
+//  ll.addFirst("Joe");
+//  System.out.println(ll);
+//  System.out.println(ll.get(1));
+//  System.out.println(ll.contains("George"));
+//  System.out.println(ll.indexOf("Joe"));
 
 
 //  System.out.println(ll.removeLast());
